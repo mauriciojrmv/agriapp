6975d5f9c77c3574de3b43d841a30ccfdbdc90c3
matias <diufram007@gmail.com>
2024-11-21 20:20:25 -0400

    Conductor Notificacion

--- app/Console/Commands/GenerarRutas.php
+++ app/Console/Commands/GenerarRutas.php
@@ -78,6 +78,7 @@ class GenerarRutas extends Command
                     //echo "Existe mas de una sola carga que satisface la capacidad por completo o casi por completo", PHP_EOL;
                     //echo "Cantidad: " . $cargasQueCumplen->count(), PHP_EOL;
                     $locations = [];
+                    $locations[] = ['lat' => $lat_mi, 'lon' => $lon_mi];
 
                     $rutaOferta  = RutaOferta::create([
                         'fecha_recogida' => Carbon::now(),
@@ -122,6 +123,7 @@ class GenerarRutas extends Command
                 } elseif ($cargasQueCumplen->count() == 1) {
                     //echo "Existe una sola carga que satisface la capacidad por completo o casi por completo", PHP_EOL;
                     $locations = [];
+                    $locations[] = ['lat' => $lat_mi, 'lon' => $lon_mi];
                     $rutaOferta  = RutaOferta::create([
                         'fecha_recogida' => Carbon::now(),
                         'capacidad_utilizada' => 0,
@@ -166,6 +168,7 @@ class GenerarRutas extends Command
                     $sumaPesoKg = $sumcargasQueCumplen->sum('pesokg');
                     if ($sumaPesoKg >= $menos10 && $sumaPesoKg <= $mas10) {
                         $locations = [];
+                        $locations[] = ['lat' => $lat_mi, 'lon' => $lon_mi];
                         $rutaOferta  = RutaOferta::create([
                             'fecha_recogida' => Carbon::now(),
                             'capacidad_utilizada' => 0,
