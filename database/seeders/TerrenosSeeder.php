a9554cd314d0abe8aa8484cab230c3b0a75a6efa
diufram <diufram007@gmail.com>
2024-12-13 10:37:29 -0400

    V1.3

--- database/seeders/TerrenosSeeder.php
+++ database/seeders/TerrenosSeeder.php
@@ -13,7 +13,7 @@ class TerrenosSeeder extends Seeder
             ['id_agricultor' => 1, 'descripcion' => 'Terreno en Plan 3000', 'area' => 120, 'superficie_total' => 120, 'ubicacion_latitud' => -17.845850, 'ubicacion_longitud' => -63.200524, 'created_at' => now(), 'updated_at' => now()],
 
             ['id_agricultor' => 2, 'descripcion' => 'Terreno en Villa Primero de Mayo', 'area' => 180, 'superficie_total' => 180, 'ubicacion_latitud' =>  -17.62928098981144, 'ubicacion_longitud' => -63.160523858636516, 'created_at' => now(), 'updated_at' => now()],
-            ['id_agricultor' => 3, 'descripcion' => 'Terreno en Pampa de la Isla', 'area' => 200, 'superficie_total' => 200, 'ubicacion_latitud' => -17.751543, 'ubicacion_longitud' => -63.116913, 'created_at' => now(), 'updated_at' => now()],
+            ['id_agricultor' => 3, 'descripcion' => 'Terreno en Pampa de la Isla', 'area' => 200, 'superficie_total' => 200, 'ubicacion_latitud' => -17.62928098981144, 'ubicacion_longitud' => -63.160523858636516, 'created_at' => now(), 'updated_at' => now()],
             ['id_agricultor' => 4, 'descripcion' => 'Terreno en Los Lotes', 'area' => 150, 'superficie_total' => 150, 'ubicacion_latitud' => -17.837489, 'ubicacion_longitud' => -63.241743, 'created_at' => now(), 'updated_at' => now()],
             ['id_agricultor' => 5, 'descripcion' => 'Terreno en Parque Industrial', 'area' => 170, 'superficie_total' => 170, 'ubicacion_latitud' => -17.811111, 'ubicacion_longitud' => -63.176571, 'created_at' => now(), 'updated_at' => now()],
             ['id_agricultor' => 6, 'descripcion' => 'Terreno en El Trompillo', 'area' => 140, 'superficie_total' => 140, 'ubicacion_latitud' => -17.790432, 'ubicacion_longitud' => -63.184478, 'created_at' => now(), 'updated_at' => now()],
